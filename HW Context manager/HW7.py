f2e56a034636507371ade79e2bc3613f9785aed4
Mikhail Chabanenko <79278609+Xundril@users.noreply.github.com>
2021-03-16 15:50:27 +0200

    Initial commit

--- HW Context manager/HW7.py
+++ HW Context manager/HW7.py
@@ -1,3 +1,5 @@
+# Task 1
+# 1 Словник елементами якого буде пара ключ:значення: де ключ - мітка часу, значення - репліка в даний момент часу
 with open("task1.txt", "r") as file:
     dict = {}
     content = file.readlines()
@@ -6,3 +8,9 @@ with open("task1.txt", "r") as file:
         content[i + 1] = content[i + 1].replace("\n", "")
         dict.update({content[i]: content[i + 1]})
     print(dict)
+
+
+# 2 Файл в якому знаходиться текст з якого видалені всі мітки часу. всі субтитри повинні мати вигляд простого тексту
+with open('homework_task1_2.txt', 'w') as file:
+    for element in dict.values():
+        file.write(f'{element} ')
