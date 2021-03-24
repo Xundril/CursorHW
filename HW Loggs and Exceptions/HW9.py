f3dc3c6bc103f1684fa0e11c037fa0072574e063
Mikhail Chabanenko <79278609+Xundril@users.noreply.github.com>
2021-03-24 23:28:45 +0200

    Done

--- HW Loggs and Exceptions/HW9.py
+++ HW Loggs and Exceptions/HW9.py
@@ -91,81 +91,81 @@ while True:
 # 0 відсотків заряду - пилосос кричить щоб його занесли на зарядку бо сам доїхати не може)
 #
 # можете придумати ще свої ексепшини і як їх опрацьовувати
-#
-# from time import sleep
-#
-# class Low_Battery(Exception):
-#     pass
-#
-# class Empty_Battery(Exception):
-#     pass
-#
-# class Lack_of_Water(Exception):
-#     pass
-#
-# class Cell(Exception):
-#     pass
-#
-# class Robot_Cleaner:
-#     battery_discharge = 5
-#     water_consumption = 10
-#     filling_the_cell = 10
-#
-#     def __init__(self):
-#         self.battery_charge = 100
-#         self.garbage_cell = 0
-#         self.amount_of_water = 100
-#
-#     def move(self):
-#         print('Loading...')
-#         while True:
-#             sleep(1)
-#             try:
-#                 print(f'Vacuum cleaner moves \n'
-#                       f'battery charge - {self.battery_charge} % \n')
-#                 self.battery_charge -= self.battery_discharge
-#                 if self.battery_charge <= 20:
-#                     if self.battery_charge == 0:
-#                         raise Empty_Battery
-#                     raise Low_Battery
-#             except Low_Battery:
-#                 print(f'!Warning! Low battery - {self.battery_charge} % \n'
-#                       '!!!Need charging!!! \n')
-#             except Empty_Battery:
-#                 print(f'Battery is discharged!')
-#                 break
-#
-#             try:
-#                 print('Vacuum cleaner working')
-#                 self.vacuum_cleaner()
-#                 if self.garbage_cell == 100:
-#                     raise Cell
-#             except Cell:
-#                 print('Garbage cell is full')
-#                 input('Press "Enter" to clean cell... ')
-#                 print()
-#                 self.garbage_cell = 0
-#
-#             try:
-#                 if self.amount_of_water > 0:
-#                     print('Vacuum cleaner washes')
-#                     self.wash()
-#                 else:
-#                     raise Lack_of_Water
-#             except Lack_of_Water:
-#                 print('!Warning! Run out of water \n')
-#
-#     def wash(self):
-#         self.amount_of_water -= self.water_consumption
-#         print(f'Water level - {self.amount_of_water} % \n')
-#
-#     def vacuum_cleaner(self):
-#         self.garbage_cell += self.filling_the_cell
-#         print(f'Garbage level  - {self.garbage_cell} % \n')
 
-#
-# robot = Robot_Cleaner()
-# robot.move()
+from time import sleep
+
+class Low_Battery(Exception):
+    pass
+
+class Empty_Battery(Exception):
+    pass
+
+class Lack_of_Water(Exception):
+    pass
+
+class Cell(Exception):
+    pass
+
+class Robot_Cleaner:
+    battery_discharge = 5
+    water_consumption = 10
+    filling_the_cell = 10
+
+    def __init__(self):
+        self.battery_charge = 100
+        self.garbage_cell = 0
+        self.amount_of_water = 100
+
+    def move(self):
+        print('Loading...')
+        while True:
+            sleep(1)
+            try:
+                print(f'Vacuum cleaner moves \n'
+                      f'battery charge - {self.battery_charge} % \n')
+                self.battery_charge -= self.battery_discharge
+                if self.battery_charge <= 20:
+                    if self.battery_charge == 0:
+                        raise Empty_Battery
+                    raise Low_Battery
+            except Low_Battery:
+                print(f'!Warning! Low battery - {self.battery_charge} % \n'
+                      '!!!Need charging!!! \n')
+            except Empty_Battery:
+                print(f'Battery is discharged!')
+                break
+
+            try:
+                print('Vacuum cleaner working')
+                self.vacuum_cleaner()
+                if self.garbage_cell == 100:
+                    raise Cell
+            except Cell:
+                print('Garbage cell is full')
+                input('Press "Enter" to clean cell... ')
+                print()
+                self.garbage_cell = 0
+
+            try:
+                if self.amount_of_water > 0:
+                    print('Vacuum cleaner washes')
+                    self.wash()
+                else:
+                    raise Lack_of_Water
+            except Lack_of_Water:
+                print('!Warning! Run out of water \n')
+
+    def wash(self):
+        self.amount_of_water -= self.water_consumption
+        print(f'Water level - {self.amount_of_water} % \n')
+
+    def vacuum_cleaner(self):
+        self.garbage_cell += self.filling_the_cell
+        print(f'Garbage level  - {self.garbage_cell} % \n')
+
+
+robot = Robot_Cleaner()
+robot.move()
 
 # Output:
 
